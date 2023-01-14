e963f9d0bea13bb53d4c962bd6cffb0ea069643a
Mauro <mauro@sdf.org>
2023-01-14 18:06:10 -0300

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="activitystreams2",
-    version="0.0.2",
+    version="0.0.3",
     description="A library that allows creation of ActivityStreams 2.0 Types",
     long_description=README,
     long_description_content_type="text/markdown",
